1cd121ac1b87e2c7667c37ec0efe0f4fa0ee145d
Jaya Wijono <jayzzwijono@yahoo.com>
2024-10-21 16:24:48 +1100

    remove redundant comment

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,6 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const request = data.toString();
