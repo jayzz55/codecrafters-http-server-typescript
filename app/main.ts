f1ce577f645a0e28297588f853bdbce52671ee37
Jaya Wijono <jayzzwijono@yahoo.com>
2024-10-22 02:15:27 +1100

    compression headers

--- app/main.ts
+++ app/main.ts
@@ -4,11 +4,11 @@ import fs from "fs";
 
 type Request = {
   method: string;
-  httpVersion: string;
   path: string;
   query: string;
   userAgent: string;
   body: string;
+  acceptEncoding: EncodingSchemeKeys | undefined;
 }
 
 const HttpMethod = {
@@ -16,6 +16,20 @@ const HttpMethod = {
   Post: 'POST'
 } as const
 type HttpMethodKeys = typeof HttpMethod[keyof typeof HttpMethod]
+
+const HttpStatus = {
+  Ok: '200 OK',
+  NotFound: '404 Not Found',
+  Created: '201 Created'
+} as const
+type HttpStatusKeys = typeof HttpStatus[keyof typeof HttpStatus]
+
+const EncodingScheme = {
+  Gzip: 'gzip',
+} as const
+type EncodingSchemeKeys = typeof EncodingScheme[keyof typeof EncodingScheme]
+
+const capitalize = (word: string): string => word.charAt(0).toUpperCase() + word.slice(1);
 
 function readRequest(data: Buffer): Request {
   const request = data.toString();
@@ -26,55 +40,84 @@ function readRequest(data: Buffer): Request {
   const body = headers[headers.length - 1]
   const userAgentHeader = headers.find(header => header.includes('User-Agent')) 
   const userAgent = userAgentHeader?.replace('User-Agent: ', '') ?? ''
+  const acceptEncodingHeader = headers.find(header => header.includes('Accept-Encoding')) 
+  const acceptEncodingString = acceptEncodingHeader?.replace('Accept-Encoding: ', '')
+  const acceptEncoding = (acceptEncodingString && capitalize(acceptEncodingString) in EncodingScheme) ? acceptEncodingString as 'gzip' : undefined
 
   return {
-    httpVersion: 'HTTP/1.1',
     method,
     path,
     query,
     userAgent,
-    body
+    body,
+    acceptEncoding
   }
+}
+
+type Response = {
+  status: HttpStatusKeys;
+  contentLength?: number;
+  contentType?: string;
+  body?: string | Buffer;
+  contentEncoding?: EncodingSchemeKeys;
+}
+
+function buildResponse(data: Response): string {
+  const contentLengthString = (data.contentLength && data.contentLength > 0) ? `Content-Length: ${data.contentLength}\r\n` : '';
+  const contentTypeString = (data.contentType && data.contentType !== '') ? `Content-Type: ${data.contentType}\r\n` : '';
+  const contentEncodingString = (data.contentEncoding && capitalize(data.contentEncoding) in EncodingScheme) ? `Content-Encoding: ${data.contentEncoding}\r\n` : '';
+
+  return `HTTP/1.1 ${data.status}\r\n` + contentTypeString + contentLengthString + contentEncodingString + "\r\n" + `${data.body}`
 }
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const request = readRequest(data)
     const directory = process.argv[3];
+    const contentEncoding = request.acceptEncoding
 
     switch (`${request.method}:${request.path}`) {
       case `${HttpMethod.Get}:/`:
-        socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+        socket.write(buildResponse({ status: HttpStatus.Ok }))
         break;
-      case `${HttpMethod.Get}:/echo/${request.query}`:
-        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${request.query.length}\r\n\r\n${request.query}`)
+      case `${HttpMethod.Get}:/echo/${request.query}`: {
+        const contentLength = request.query.length;
+        const body = request.query;
+
+        socket.write(buildResponse({ status: HttpStatus.Ok, contentType: 'text/plain', contentLength, contentEncoding, body }))
         break;
-      case `${HttpMethod.Get}:/user-agent`:
-        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${request.userAgent.length}\r\n\r\n${request.userAgent}`)
+      }
+      case `${HttpMethod.Get}:/user-agent`: {
+        const contentLength = request.userAgent.length;
+        const body = request.userAgent;
+
+        socket.write(buildResponse({ status: HttpStatus.Ok, contentType: 'text/plain', contentLength, contentEncoding, body }))
         break;
+      }
       case `${HttpMethod.Get}:/files/${request.query}`:  {
-          const filePath = `${directory}/${request.query}`;
+        const filePath = `${directory}/${request.query}`;
 
-          if (fs.existsSync(filePath)) {
-            const content = fs.readFileSync(filePath)
+        if (fs.existsSync(filePath)) {
+          const content = fs.readFileSync(filePath)
+          const contentLength = content.length;
+          const body = fs.readFileSync(filePath)
 
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
-          } else {
-            socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
-          }
-          break;
+          socket.write(buildResponse({ status: HttpStatus.Ok, contentType: 'application/octet-stream', contentLength, contentEncoding, body }))
+        } else {
+          socket.write(buildResponse({ status: HttpStatus.NotFound }))
         }
+        break;
+      }
       case `${HttpMethod.Post}:/files/${request.query}`: {
-          const filePath = `${directory}/${request.query}`;
-          fs.writeFileSync(filePath, request.body);
-          socket.write(Buffer.from(`HTTP/1.1 201 Created\r\n\r\n`));
-          break;
-        }
+        const filePath = `${directory}/${request.query}`;
+        fs.writeFileSync(filePath, request.body);
+        socket.write(buildResponse({ status: HttpStatus.Created }))
+        break;
+      }
       default:
-        socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
+        socket.write(buildResponse({ status: HttpStatus.NotFound }))
         break;
     }
-
     socket.end();
   })
 });
