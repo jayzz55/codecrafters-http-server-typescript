40dde61398deabeed4ed53e684579c8a1e95773b
Jaya Wijono <jayzzwijono@yahoo.com>
2024-10-21 14:57:51 +1100

    extract URL path

--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,14 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-  socket.end();
+  socket.on('data', (data) => {
+    const request = data.toString();
+    const path = request.split(' ')[1]
+    const response = path === '/' ? '200 OK' : '404 Not Found'
+
+    socket.write(Buffer.from(`HTTP/1.1 ${response}\r\n\r\n`));
+    socket.end();
+  })
 });
 
 server.listen(4221, "localhost");
